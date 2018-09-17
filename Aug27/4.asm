37921a6baceb285696bcd6b19ec0c87ed3f36681
s10singh97 <s10singh97@yahoo.in>
2018-09-17 09:20:01 +0530

    Sum of first n natural numbers

--- Aug27/4.asm
+++ Aug27/4.asm
@@ -1,11 +1,7 @@
 ;h-1,l-0 
 lda 2000
-MOV B, A
-ANI 0F
-STA 2004
-MOV A, B
-ANI 0F
-RLC
-STA 2005
-
+mvi b,00H
+ana B
+mvi b,0F0H
+ora B
 hlt
---
+++ Sept17/1.asm~
@@ -0,0 +1,10 @@
+
+;Sum of first n numbers
+mvi a, 5; n = 5
+mov c,a
+dcr c
+loop: jz stop
+add c
+dcr c
+jmp loop
+stop: hlt
